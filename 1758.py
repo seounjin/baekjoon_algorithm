70cb03198218bbff429a52ae8bb431de14703bda
seounjin <invanda7@gamil.com>
2020-03-08 19:29:55 +0900

    그리디

---
+++ 1758.py
@@ -0,0 +1,20 @@
+# 알바생 강호
+import sys
+
+N = int(sys.stdin.readline())
+
+array = []
+for _ in range(N):
+    array.append(int(sys.stdin.readline()))
+
+# 월래 주려는 돈 - (받은등수 - 1)
+# 돈이 큰순서로 정렬
+array.sort(reverse=True)
+
+answer = 0
+
+for index in range(N):
+    if array[index] - i > 0:
+        answer += array[index] - i
+
+print(answer)
